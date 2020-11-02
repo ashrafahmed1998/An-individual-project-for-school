e04ae88bfab1bf2e15a78a873fb4ca317f9adad4
Ashraf Ahmed <ashraf.ahmed19981998@gmail.com>
2020-11-02 20:22:49 +0200

    rating

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,19 +1,20 @@
 import React from 'react'
 
-const Review = ({value}) => {
+const Review = ({value,color}) => {
     return (
         <>
-            <i className={ value >=1 ? 'fas fa-star': value >= 0.5 ? 'fad fa-start-half-alt' : value ===0 ? 'fal fa-star': ''} />
-            <i className={ value >=1 ? 'fas fa-star': value >= 0.5 ? 'fad fa-start-half-alt' : value ===0 ? 'fal fa-star': ''} />
-            <i className={ value >=1 ? 'fas fa-star': value >= 0.5 ? 'fad fa-start-half-alt' : value ===0 ? 'fal fa-star': ''} />
-            <i className={ value >=1 ? 'fas fa-star': value >= 0.5 ? 'fad fa-start-half-alt' : value ===0 ? 'fal fa-star': ''} />
-            <i className={ value >=1 ? 'fas fa-star': value >= 0.5 ? 'fad fa-start-half-alt' : value ===0 ? 'fal fa-star': ''} />
+            <i className={value >=1 ? 'fas fa-star': value >= 0.5 ? 'fas fa-star-half-alt' : 'far fa-star'} style={{color}} />
+            <i className={value >=2 ? 'fas fa-star': value >= 1.5 ? 'fas fa-star-half-alt' : 'far fa-star'} style={{color}} />
+            <i className={value >=3 ? 'fas fa-star': value >= 2.5 ? 'fas fa-star-half-alt' :'far fa-star'} style={{color}}/>
+            <i className={value >=4 ? 'fas fa-star': value >= 3.5 ? 'fas fa-star-half-alt' : 'far fa-star'} style={{color}} />
+            <i className={value >=5 ? 'fas fa-star': value>= 4.5? 'fas fa-star-half-alt': 'far fa-star'} style={{color}} />
         </>
     )
 }
 
+Review.defaultProps={
+    color:'#f8e825'
+}
+
 export default Review
 
-// Review.defaultProps={
-//     color: yellow
-// }
